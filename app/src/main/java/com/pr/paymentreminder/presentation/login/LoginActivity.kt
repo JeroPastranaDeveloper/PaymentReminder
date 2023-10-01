de8ae0fde20a544d0ef9fe8efb6a8bcaa16ede97
JeroPastranaDeveloper <127027336+JeroPastranaDeveloper@users.noreply.github.com>
2023-10-01 14:06:50 +0200

    Building structure

--- app/src/main/java/com/pr/paymentreminder/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/pr/paymentreminder/presentation/login/LoginActivity.kt
@@ -1,10 +1,12 @@
-package com.pr.paymentreminder
+package com.pr.paymentreminder.presentation.login
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
