1e4d86d6737fa3d306eaf1465551733de9b17dae
Jero Pastrana <jeronimo.pastrana@gigigo.com>
2023-10-06 13:14:11 +0200

    - Creating Bottom Sheet Dialog

--- app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/fragments/HomeFragment.kt
+++ app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/fragments/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.pr.paymentreminder.presentation.paymentreminder.fragments
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,10 +14,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomSheetState
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,9 +44,15 @@ import com.pr.paymentreminder.ui.theme.dimen56
 import com.pr.paymentreminder.ui.theme.emptyString
 import com.pr.paymentreminder.ui.theme.spacing4
 import com.pr.paymentreminder.ui.theme.spacing8
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeFragment(viewModel: HomeViewModel) {
+    val coroutineScope = rememberCoroutineScope()
+    var bottomSheetState: BottomSheetState = rememberBottomSheetState(BottomSheetValue.Collapsed)
+    var serviceText by remember { mutableStateOf(emptyString()) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -42,18 +60,39 @@ fun HomeFragment(viewModel: HomeViewModel) {
         verticalArrangement = Arrangement.SpaceEvenly,
     ) {
         for (service in viewModel.services.value) {
-            ServiceCard(service)
+            ServiceCard(service) { clickedService ->
+                serviceText = clickedService.name
+                coroutineScope.launch {
+                    bottomSheetState.expand()
+                }
+            }
         }
         Spacer(modifier = Modifier.height(dimen56))
     }
+
+    BottomSheet(
+        sheetState = bottomSheetState,
+        sheetContent = {
+            TextField(
+                value = serviceText,
+                onValueChange = { serviceText = it },
+                label = { Text("Nombre del servicio") },
+                modifier = Modifier.padding(spacing8)
+            )
+        }
+    )
 }
 
 @Composable
-private fun ServiceCard(service: Service) {
+private fun ServiceCard(
+    service: Service,
+    onCardClick: (service) -> Unit
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(spacing8)
+            .clickable { onCardClick(service) }
     ) {
         Row(
             modifier = Modifier
