fbd9a8344c2f17a4a7a83322d5048f56aa1c918f
Jero Pastrana <jeronimo.pastrana@gigigo.com>
2023-10-26 14:54:33 +0200

    - Finished bottom sheet appearance

--- app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/fragments/HomeFragment.kt
+++ app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/fragments/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,10 +19,15 @@ import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -48,7 +54,9 @@ import com.pr.paymentreminder.ui.theme.dimen16
 import com.pr.paymentreminder.ui.theme.dimen56
 import com.pr.paymentreminder.ui.theme.dimen8
 import com.pr.paymentreminder.ui.theme.emptyString
+import com.pr.paymentreminder.ui.theme.spacing16
 import com.pr.paymentreminder.ui.theme.spacing4
+import com.pr.paymentreminder.ui.theme.spacing72
 import com.pr.paymentreminder.ui.theme.spacing8
 import java.util.Calendar
 
@@ -56,23 +64,38 @@ import java.util.Calendar
 fun HomeFragment(viewModel: HomeViewModel) {
     var selectedService by remember { mutableStateOf<Service?>(null) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.SpaceEvenly,
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        viewModel.services.value.map { service ->
-            ServiceCard(service) {
-                selectedService = service
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            viewModel.services.value.map { service ->
+                ServiceCard(service) {
+                    selectedService = service
+                }
             }
         }
-        Spacer(modifier = Modifier.height(dimen56))
-    }
 
-    selectedService?.let {
-        ServiceBottomSheet(service = it, viewModel) {
-            selectedService = null
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = spacing72, end = spacing16),
+            onClick = { /* TODO */ }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = emptyString()
+            )
+        }
+
+        selectedService?.let {
+            ServiceBottomSheet(service = it, viewModel) {
+                selectedService = null
+            }
         }
     }
 }
@@ -81,14 +104,19 @@ fun HomeFragment(viewModel: HomeViewModel) {
 @Composable
 private fun ServiceBottomSheet(service: Service?, viewModel: HomeViewModel, onDismiss: () -> Unit) {
     var serviceName by remember { mutableStateOf(TextFieldValue(service?.name ?: emptyString())) }
+    var servicePrice by remember { mutableStateOf(TextFieldValue(service?.price ?: emptyString())) }
 
     var selectedCategory by remember { mutableStateOf(service?.category ?: emptyString()) }
     val categories = listOf(Categories.AMAZON, Categories.HOBBY, Categories.PLATFORMS)
     var categoriesExpanded by remember { mutableStateOf(false) }
 
-    var selectedPaymentType by remember { mutableStateOf(service?.type ?: emptyString())}
+    var selectedPaymentType by remember { mutableStateOf(service?.type ?: emptyString()) }
     val types = listOf(PaymentType.WEEKLY, PaymentType.MONTHLY, PaymentType.YEARLY)
     var typesExpanded by remember { mutableStateOf(false) }
+
+    var selectedRemember by remember { mutableStateOf(service?.remember ?: emptyString()) }
+    val daysRemember = listOf(1, 2, 3)
+    var daysExpanded by remember { mutableStateOf(false) }
 
     ModalBottomSheetLayout(
         sheetContent = {
@@ -129,6 +157,7 @@ private fun ServiceBottomSheet(service: Service?, viewModel: HomeViewModel, onDi
                             }
                         }
                     }
+
                     val context = LocalContext.current
                     viewModel.checkDateText(service)
                     val datePickerDialog = remember {
@@ -138,9 +167,9 @@ private fun ServiceBottomSheet(service: Service?, viewModel: HomeViewModel, onDi
                                 // TODO: COMPROBAR EL VIEWMODEL Y ETC
                                 viewModel.updateDate("${dayOfMonth}/${month + 1}/${year}")
                             },
-                            Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+                            Calendar.getInstance().get(Calendar.YEAR),
                             Calendar.getInstance().get(Calendar.MONTH),
-                            Calendar.getInstance().get(Calendar.YEAR)
+                            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
                         )
                     }
                     Text(
@@ -161,7 +190,6 @@ private fun ServiceBottomSheet(service: Service?, viewModel: HomeViewModel, onDi
                         .clickable {
                             typesExpanded = !typesExpanded
                         }
-                        .weight(0.5f)
                 )
                 if (typesExpanded) {
                     DropdownMenu(
@@ -181,6 +209,52 @@ private fun ServiceBottomSheet(service: Service?, viewModel: HomeViewModel, onDi
                 }
 
                 Spacer(modifier = Modifier.height(dimen16))
+
+                TextField(
+                    value = servicePrice,
+                    onValueChange = { servicePrice = it },
+                    label = { Text(stringResource(id = R.string.service_price)) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(dimen16))
+
+                Text(
+                    stringResource(id = R.string.remember_days_before, selectedRemember),
+                    modifier = Modifier
+                        .clickable {
+                            daysExpanded = !daysExpanded
+                        }
+                )
+                if (daysExpanded) {
+                    DropdownMenu(
+                        expanded = daysExpanded,
+                        onDismissRequest = { daysExpanded = false },
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        daysRemember.forEach { day ->
+                            DropdownMenuItem(onClick = {
+                                selectedRemember = day.toString()
+                                daysExpanded = false
+                            }) {
+                                Text(text = day.toString())
+                            }
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(dimen16))
+
+                Button(
+                    onClick = {
+                        // TODO
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(text = stringResource(id = R.string.btn_save))
+                }
+
                 Spacer(modifier = Modifier.height(dimen56))
             }
         },
