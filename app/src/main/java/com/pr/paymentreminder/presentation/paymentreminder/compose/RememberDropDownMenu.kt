c2bb77491a4b8ce7f0081273f6c5f9ae654f656a
Jero Pastrana <jeronimo.pastrana@gigigo.com>
2024-01-05 13:47:17 +0100

    - Refactor: Refactored code

--- app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/compose/RememberDropDownMenu.kt
+++ app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/compose/RememberDropDownMenu.kt
@@ -33,7 +33,7 @@ fun RememberDropDownMenu(
     val hasHelperText by textHelper.collectAsState(null)
 
     Text(
-        text = stringResource(id = R.string.remember_days_before, selectedDay),
+        text = stringResource(id = if (selectedDay == rememberDays[0].toString()) R.string.remember_day_before else R.string.remember_days_before, selectedDay),
         modifier = Modifier.clickable { daysExpanded = !daysExpanded }
     )
 
@@ -80,7 +80,7 @@ private fun RememberDropDownMenuHelperTextPreview() {
     Column {
         RememberDropDownMenu(
             rememberDays = listOf(1, 2, 3),
-            initialSelectedDay = "2",
+            initialSelectedDay = "1",
             textHelper = MutableStateFlow(emptyString()),
             textHelperText = stringResource(id = R.string.invalid_service_remember)
         ) { /* Nothing */ }
