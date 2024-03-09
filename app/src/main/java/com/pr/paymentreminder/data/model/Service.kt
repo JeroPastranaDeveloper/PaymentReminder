21355466551abd029f03b341d2ac2f22e4108e29
Jero Pastrana <127027336+JeroPastranaDeveloper@users.noreply.github.com>
2024-03-09 16:38:29 +0100

    - Refactor: Refactored Service model - In progress: AddServiceActivity
    - Bug: Can't get services when you sign out and login again
    - Bug: Can't remove services properly

--- app/src/main/java/com/pr/paymentreminder/data/model/Service.kt
+++ app/src/main/java/com/pr/paymentreminder/data/model/Service.kt
@@ -1,14 +1,16 @@
 package com.pr.paymentreminder.data.model
 
+import com.pr.paymentreminder.ui.theme.emptyString
+
 data class Service(
-    var id: String,
-    val category: String,
-    val color: String,
-    var date: String,
-    var name: String,
-    val price: String,
-    val remember: String,
-    val type: String,
-    val image: String,
-    val url: String?
+    var id: String = emptyString(),
+    val category: String = emptyString(),
+    val color: String = emptyString(),
+    var date: String = emptyString(),
+    var name: String = emptyString(),
+    val price: String = emptyString(),
+    val remember: String = emptyString(),
+    val type: String = emptyString(),
+    val image: String = emptyString(),
+    val url: String? = emptyString()
 )
