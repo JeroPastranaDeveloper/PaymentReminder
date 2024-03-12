165abd7d023bef2b0e5a8cd572238cc4a3ed8a71
Jero Pastrana <127027336+JeroPastranaDeveloper@users.noreply.github.com>
2024-03-12 11:51:50 +0100

    - In progress: AddServiceActivity - Refactor: Refactored code
    - Bug: Can't get services when you sign out and login again

--- app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/compose/CategoriesDropDownMenu.kt
+++ app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/compose/CategoriesDropDownMenu.kt
@@ -2,6 +2,8 @@ package com.pr.paymentreminder.presentation.paymentreminder.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.pr.paymentreminder.R
 import com.pr.paymentreminder.data.model.Categories
+import com.pr.paymentreminder.ui.theme.dimen16
 import com.pr.paymentreminder.ui.theme.emptyString
 
 @Composable
@@ -34,7 +37,7 @@ fun CategoriesDropDownMenu(
     )
 
     if (hasHelperText) {
-        ServiceSeparator()
+        Spacer(modifier = Modifier.height(dimen16))
         HelperText(textHelperText)
     }
 
