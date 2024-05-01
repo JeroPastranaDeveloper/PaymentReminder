e6fc78573a2c93b2c49a8abdb669727617b42380
Jero Pastrana <127027336+JeroPastranaDeveloper@users.noreply.github.com>
2024-05-01 18:01:45 +0200

    - Remove: Removed useless notification room :')

--- app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/fragments/settings/SettingsViewModel.kt
+++ app/src/main/java/com/pr/paymentreminder/presentation/paymentreminder/fragments/settings/SettingsViewModel.kt
@@ -2,6 +2,7 @@ package com.pr.paymentreminder.presentation.paymentreminder.fragments.settings
 
 import androidx.lifecycle.viewModelScope
 import com.pr.paymentreminder.base.BaseComposeViewModelWithActions
+import com.pr.paymentreminder.data.preferences.PreferencesHandler
 import com.pr.paymentreminder.domain.usecase.login.SignOutUseCase
 import com.pr.paymentreminder.domain.usecase.service_form.ClearAllServiceFormsUseCase
 import com.pr.paymentreminder.presentation.paymentreminder.fragments.settings.SettingsViewContract.UiAction
@@ -15,6 +16,7 @@ import javax.inject.Inject
 class SettingsViewModel @Inject constructor(
     private val signOutUseCase: SignOutUseCase,
     private val clearAllServiceForms: ClearAllServiceFormsUseCase,
+    private val preferencesHandler: PreferencesHandler
 ) : BaseComposeViewModelWithActions<UiState, UiIntent, UiAction>() {
     override val initialViewState = UiState()
     override fun manageIntent(intent: UiIntent) {
@@ -27,6 +29,7 @@ class SettingsViewModel @Inject constructor(
     private fun signOut() {
         viewModelScope.launch {
             clearAllServiceForms()
+            preferencesHandler.firstTime = true
             signOutUseCase()
         }
     }
